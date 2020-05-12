e5210019e4b63f11d274967b80d403b7f65c4346
manuelmpouras <65111922+manuelmpouras@users.noreply.github.com>
2020-05-12 21:14:05 +0200

    Update sn.sh

--- sn.sh
+++ sn.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 sub_new()
 {
+echo "$1"
+git init remote "$1"
+#git push "$1"
+  if ! [ "$1" ] ; then
+    git init -q || error 'Unable to initialize Git directory'
+  fi
 
-echo "$1" "$2"
 
 }
 
@@ -11,6 +16,6 @@ net_name="$2"
 shift
 case "$subcommand" in
   create)
-  sub_new "$subcommand" "$net_name"
+  sub_new "$@"
   ;;
   esac
