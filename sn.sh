4f554fca81c02238ebf38811e0b108c4ad317c60
manuelmpouras <65111922+manuelmpouras@users.noreply.github.com>
2020-05-13 18:18:15 +0200

    Update sn.sh

--- sn.sh
+++ sn.sh
@@ -5,7 +5,7 @@ echo "$1"
 ##Creating a local Social Network (git repository) in the current directory
   if [ "$1" ] ; then
     git init $(pwd)"/""$1"
-	cd $(pwd)"/""$1"
+	echo $(whoami)>>$(pwd)"/"$1"/users.log"
 	echo $(whoami)
   else
 	echo "Please define the name of the social network"
@@ -20,14 +20,18 @@ echo "$1"
 ##Joining in an already existing social network (git repository) and cloning it in the current directory
   if [ "$1" ] ; then
     git clone https://github.com/manuelmpouras/CoolNetwork
-	cd $(pwd)"/""CoolNetwork"
-	echo $(whoami)
-	echo $0 $1
+	echo $(whoami)>>$(pwd)"/CoolNetwork/users.log"
+	cd $(pwd)"/CoolNetwork/"
+	git add "users.log"
+	git commit -m "New member registered"
+	git remote add origin https://github.com/manuelmpouras/CoolNetwork
+	git push -u origin master
+	echo $(pwd)"/CoolNetwork/users.log"
+	cd ..
   else
 	echo "Please define the name of the social network"
 
   fi
-
 }
 subcommand="$1"
 net_name="$2"
