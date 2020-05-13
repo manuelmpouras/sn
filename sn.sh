f684f12753d5a2057f8a194c38057938e888bc1f
manuelmpouras <65111922+manuelmpouras@users.noreply.github.com>
2020-05-13 10:57:43 +0200

    Update sn.sh

--- sn.sh
+++ sn.sh
@@ -2,23 +2,41 @@
 sub_new()
 {
 echo "$1"
-cd
-git init "$1"
-cd ./"$1"
-git remote add origin "https://github.com/manuelmpouras/"$1
-git push -u origin master
-  if ! [ "$1" ] ; then
-    git init -q || error 'Unable to initialize Git directory'
-  fi
+##Creating a local Social Network (git repository) in the current directory
+  if [ "$1" ] ; then
+    git init $(pwd)"/""$1"
+	cd $(pwd)"/""$1"
+	echo $(whoami)
+  else
+	echo "Please define the name of the social network"
 
+  fi
 
 }
 
+sub_join()
+{
+echo "$1"
+##Joining in an already existing social network (git repository) and cloning it in the current directory
+  if [ "$1" ] ; then
+    git clone https://github.com/manuelmpouras/CoolNetwork
+	cd $(pwd)"/""CoolNetwork"
+	echo $(whoami)
+	echo $0 $1
+  else
+	echo "Please define the name of the social network"
+
+  fi
+
+}
 subcommand="$1"
 net_name="$2"
 shift
 case "$subcommand" in
   create)
   sub_new "$@"
+  ;;
+  join)
+  sub_join "$@"
   ;;
   esac
