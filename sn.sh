59b99f9e72729bb5d84c638f14e7e449711c6aa5
manuelmpouras <65111922+manuelmpouras@users.noreply.github.com>
2020-05-13 19:54:11 +0200

    Update sn.sh

--- sn.sh
+++ sn.sh
@@ -25,13 +25,44 @@ sub_join()
 	git commit -m "New member registered"
 	git remote add origin https://github.com/manuelmpouras/CoolNetwork
 	git push -u origin master
-	echo $(pwd)"/CoolNetwork/users.log"
-	cd ..
+	if [ $? -ne 0 ] ; then
+		echo Oooops. Registration failed. I am sorry you should login in your github\' account.
+		rm -rf ../CoolNetwork/
+	else
+		cd ..
+	fi
   else
 	echo "You have already been registered. Thank you for joining our CoolNetwork."
 
   fi
 }
+
+sub_show()
+{
+##Joining in an already existing social network (git repository) and cloning it in the current directory
+git log --pretty=format:%an |
+sort |
+uniq && echo " THE MEMBERS REGISTERED"
+
+}
+
+sub_help()
+{
+## In case of typing anything other than a specified argument of sn, then help page will be displayed 
+echo "|
+Start an issue repository
+  join			Join our CoolNetwork, |
+			clones it in your curent directory
+			and registers you with your github's account.
+			
+  create		Create a new social network a with the name 
+			defined as the second argument 
+			in your current directory.
+			
+  show_members		Shows the names 
+			registed in our CoolNetwork."
+}
+
 subcommand="$1"
 net_name="$2"
 shift
@@ -42,4 +73,15 @@ case "$subcommand" in
   join)
   sub_join "$@"
   ;;
+  show_members)
+  sub_show
+  ;;
+  help)
+  sub_help
+  ;;
+    *)
+  sub_help
+  exit 1
+  ;;
+
   esac
