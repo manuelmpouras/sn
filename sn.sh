08cd1d07dd16e15640cda2199f69b5439b0683a9
manuelmpouras <65111922+manuelmpouras@users.noreply.github.com>
2020-05-14 17:51:05 +0200

    Update sn.sh

--- sn.sh
+++ sn.sh
@@ -39,11 +39,48 @@ sub_join()
 
 sub_show()
 {
-##Joining in an already existing social network (git repository) and cloning it in the current directory
-git log --pretty=format:%an |
-sort |
-uniq && echo " THE MEMBERS REGISTERED"
+##Show all the members registered in the CoolNetwork
+if ! [ -d CoolNetwork/ ] ; then
+	git clone https://github.com/manuelmpouras/CoolNetwork
+	cd $(pwd)"/CoolNetwork/"
+	git log --pretty=format:%an |
+	sort |
+	uniq && echo " THE MEMBERS REGISTERED"
+else
+	cd $(pwd)"/CoolNetwork/"
+	git log --pretty=format:%an |
+	sort |
+	uniq && echo " THE MEMBERS REGISTERED"
+fi
+}
+sub_pull()
+{
+##Pull all the new posts and likes of the CoolNetwork
+	if [ -d CoolNetwork/ ] ; then
+		rm -rf CoolNetwork/
+	fi
+	git clone https://github.com/manuelmpouras/CoolNetwork
+	echo " Posts and likes pulled correctly"
+	echo " You can see all of them in your local CoolNetwork directory"
+}
 
+sub_log()
+{
+##List all the posts and likes of the CoolNetwork
+	if ! [ -d CoolNetwork/ ] ; then
+		echo Plese first pull the latest changes with /sn pull/
+	else
+		cd $(pwd)"/CoolNetwork/"
+		find . -name "*.post" |
+		sort |
+		uniq |
+		while read post ; do
+#			The number of likes is reported in the third line of the file "$post.info"
+			name_post= echo -n $post | cut -d '/' -f 2 | tr -d '\n'
+			echo -n " is a post with number of likes " && sed -n 3p "$post.info" 
+		done
+		echo " You can see all of them in your local CoolNetwork directory"
+	fi
 }
 
 sub_help()
@@ -51,16 +88,27 @@ sub_help()
 ## In case of typing anything other than a specified argument of sn, then help page will be displayed 
 echo "|
 Start an issue repository
-  join			Join our CoolNetwork, |
+
+  join			Join our CoolNetwork, 
 			clones it in your curent directory
 			and registers you with your github's account.
+			
 			
   create		Create a new social network a with the name 
 			defined as the second argument 
 			in your current directory.
 			
+			
   show_members		Shows the names 
-			registed in our CoolNetwork."
+			registed in our CoolNetwork.
+			
+			
+  pull			Pull in your local directory 
+			all the posts and likes of the CoolNetwork.
+			
+			
+  log			List all the posts and likes of the CoolNetwork."
+
 }
 
 subcommand="$1"
@@ -75,6 +123,12 @@ case "$subcommand" in
   ;;
   show_members)
   sub_show
+  ;;
+  pull)
+  sub_pull
+  ;;
+  log)
+  sub_log
   ;;
   help)
   sub_help
